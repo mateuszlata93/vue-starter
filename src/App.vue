c2e48f9df3fa98fab440452dbba757ecc2b27eaa
lata <mateuszlata93@gmail.com>
2023-04-16 11:13:45 +0200

    add login/logout button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="my-app">
-    <h1>Hello  {{email}}$!</h1>
-    <label>Login with email</label>
-    <input type="email" v-model="email">
-    <div v-if="email.length < 10">This is your email length: {{email.length}}</div>
-    <div v-if="email.length < 10">Your address is too short!</div>
-    <div v-if="!email.includes('@')">Your address does not contain @</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    <button @click="alertMyEmail()">Display my email address in alert</button>
+    <h1>Hello  {{email}} !</h1>
+    <!-- <label>Login with email</label>
+     <input type="email" v-model="email">
+     <div>This is your email length: {{email.length}}</div>
+     <div v-if="email.length < 10">Your address is too short!</div>
+     <div v-if="!email.includes('@')">Your address does not contain @</div>
+     <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+     <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+     <button @click="alertMyEmail()">Display my email address in alert</button>-->
+    <LoginForm v-if="loggedIn == false" @login="(username) => logMeIn(username)"></LoginForm>
+    <button v-if="loggedIn" @click="logMeOut()">Wyloguj</button>
   </div>
 </template>
 
 <script>
+import "milligram";
+import LoginForm from "./LoginForm";
+
 export default {
+  components: {LoginForm},
   data() {
     return {
-      email: 'test',
-      password: 'test'
+      password: '',
+      loggedIn: false
     };
   },
   methods: {
     alertMyEmail() {
       alert(this.email);
+    },
+    logMeIn(username) {
+      this.loggedIn = true;
+      this.email = username;
+    },
+    logMeOut(){
+      this.loggedIn = false;
+      this.email = '';
     }
   }
 }
---
+++ src/LoginForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label>Zaloguj się e-mailem</label>
+    <input type="email" v-model="email">
+    <button @click="enter()">Zaloguj się</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      isHidden: false
+    }
+  },
+  methods: {
+    enter() {
+      this.$emit('login', this.email);
+    }
+  }
+}
+</script>
