fd26d7144f063b9edef5b16313612a30e1de3ce4
lata <mateuszlata93@gmail.com>
2023-04-16 10:09:56 +0200

    add email field input with validation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+    <h1>Hello  {{email}}$!</h1>
+    <label>Login with email</label>
+    <input type="email" v-model="email">
+    <div v-if="email.length < 10">This is your email length: {{email.length}}</div>
+    <div v-if="email.length < 10">Your address is too short!</div>
+    <div v-if="!email.includes('@')">Your address does not contain @</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+    <button @click="alertMyEmail()">Display my email address in alert</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      email: 'test',
+      password: 'test'
+    };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
+  }
 }
 </script>
 
